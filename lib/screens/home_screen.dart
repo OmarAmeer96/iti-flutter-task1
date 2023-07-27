e6958c700a647cb851d438b83ec78e61f65de16a
Omar_Ameer <omar.ameer244@gmail.com>
2023-07-27 16:53:59 +0300

    Commit 3

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,46 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: const Color(0xff01dbc0),
         ),
+        body: const Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Price Monitor',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat-Bold',
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.search_rounded,
+                    size: 28,
+                    color: Color(0xffc3cbd2),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Search',
+                    style: TextStyle(
+                      color: Color(0xffc3cbd2),
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat-Bold',
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
